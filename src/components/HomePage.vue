c9b62b190ddb518213c264f51d35b8d4461f1237
Gavin Sokolof <gavin@thesistesting.com>
2020-03-10 19:09:15 -0400

    created homepage, edited name with animation

---
+++ src/components/HomePage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="home-container">
+    <name-container/>
+    <div class="content-container">
+      <ul class="links-list">
+        <li class="list-item"><a href="#bio">Bio</a></li>
+        <li class="list-item"><a href="">Projects</a></li>
+        <li class="list-item"><a href="">Cooking With G</a></li>
+        <li class="list-item"><a href="">Contact Form</a></li>
+      </ul>
+      <cooking-component/>
+      <about-me/>
+      <p></p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+const CookingComponent = () => import('./CookingComponent.vue')
+const NameContainer = () => import('./NameContainer.vue')
+const AboutMe = () => import('./AboutMe.vue')
+export default {
+  name: 'HomePage',
+  components: {
+    NameContainer, AboutMe, CookingComponent
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+body {
+  width: 100%;
+  height: 100%;
+  font-size: 16px;
+  overflow-x: hidden;
+}
+.links-list {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  justify-content: space-between;
+  @media only screen and (min-width: 700px){
+    transform: translateX(396px);
+  }
+  @media only screen and (max-width: 1279px) and (min-width: 960px){
+    transform: translateX(457px);
+  }
+  @media only screen and (min-width: 1280px){
+    transform: translateX(597px);
+  }
+  .list-item {
+    margin-left: 0;
+    margin-right: 0;
+    a {
+      color: black;
+    }
+  }
+
+}
+.content-container {
+  margin-top: 100px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 150px 600px 1fr;
+  @media screen and (min-width: 1280px){
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 1240px;
+    grid-template-columns: 680px 1fr;
+    justify-content: flex-end;
+  }
+}
+.home-container {
+  height: 100%;
+  display: block;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/NameContainer.vue
@@ -0,0 +1,205 @@
+<template>
+  <div class="name-container">
+    <span class="name-letter">G</span>
+    <span class="name-letter">A</span>
+    <span class="name-letter">V</span>
+    <span class="name-letter">I</span>
+    <span class="name-letter">N</span>
+    <span class="space"></span>
+    <span class="space"></span>
+    <span class="name-letter">S</span>
+    <span class="name-letter">O</span>
+    <span class="name-letter">K</span>
+    <span class="name-letter">O</span>
+    <span class="name-letter">L</span>
+    <span class="name-letter">O</span>
+    <span class="name-letter">F</span>
+  </div>
+
+</template>
+
+<script>
+  import $ from 'jQuery'
+
+  export default {
+    data () {
+    return {
+    }
+  },
+    methods: 
+    {
+      animateLetters(){
+      }
+    },
+    mounted(){     
+      $(".name-letter").hover(function(){
+        console.log($('.name-letter>.event-log'), 'end')
+        $(this).addClass("animate")
+        setTimeout(()=> $(this).removeClass("animate"), 2010)          
+      })
+    }
+  }
+
+</script>
+
+<style lang="scss" >
+
+@import url('https://fonts.googleapis.com/css?family=Montserrat|Raleway&display=swap');
+
+.name-container {
+  display: flex;
+  flex-direction: row;
+}
+.space {
+  width: 1rem;
+}
+
+.name-letter {
+  text-transform: uppercase;
+  font-family: 'Raleway', sans-serif;
+  line-height: 90px;
+  font-weight: 700;
+  font-size: 40px;
+  @media screen and (min-width: 700px) {
+    font-size: 50px;
+  }
+  @media screen and (min-width: 960px) {
+    font-size: 60px;
+  }
+  @media screen and (min-width: 1280px) {
+    font-size: 80px;
+  }
+  background: linear-gradient(to right,  #5433FF 15%, #20BDFF 35%, #00ffff 75%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  
+  &:nth-child(2){
+    background: linear-gradient(to right, #00ffff 15%, #64b3f4 75%);
+    -webkit-background-clip: text;
+   } 
+  &:nth-child(3){
+    background: linear-gradient(to right, #64b3f4 25%, #33FF66 75%);
+    -webkit-background-clip: text;
+   }
+  &:nth-child(4){
+    background: linear-gradient(to right, #33FF66 5%, #59c173 85%);
+    -webkit-background-clip: text;
+   }
+  &:nth-child(5){ 
+    background: linear-gradient(to right, #66ff00 5%, #a80077 75%);
+    -webkit-background-clip: text;
+  }
+  &:nth-child(8){ 
+    background: linear-gradient(to right,#a80077 5%, #db36a4 35%, #FDFC47 85%);
+    -webkit-background-clip: text;
+  }
+  &:nth-child(9){
+    background: linear-gradient(to right, #FDFC47 25%, #00c9ff);
+    -webkit-background-clip: text;
+  }
+  &:nth-child(10){    
+    background: linear-gradient(to right, #00c9ff 25%, #00ffff 75%);
+    -webkit-background-clip: text;
+  }
+  &:nth-child(11){
+    background: linear-gradient(to right, #00ffff 25%, #E100FF 75%);
+    -webkit-background-clip: text;
+   }
+   &:nth-child(12){
+      background: linear-gradient(to right, #db36a4 5%, #E100FF 35%, #7F00FF 80%);
+      -webkit-background-clip: text;
+   }
+   &:nth-child(13){
+     background: linear-gradient(to right, #7F00FF 15%, #ff0084 85%);
+    -webkit-background-clip: text;
+   }
+    &:nth-child(14){
+     background: linear-gradient(to right, #ff0084, #f2709c);
+    -webkit-background-clip: text;
+   }    
+  }
+
+  .animate {
+    animation-name: moveTextUp;
+    animation-duration: 2s; 
+    animation-iteration-count: 10;
+    animation-direction: alternate;
+    animation-timing-function: linear; 
+    animation-fill-mode: forwards;
+    pointer-events: none;
+    backface-visibility: hidden;
+    perspective: 1000; 
+    &:nth-child(even){
+      animation-name: moveTextDown;
+    }
+  }
+  
+
+  @-webkit-keyframes moveTextDown {
+    0%,100%   { transform: translate3d(0%, 0px, 0px); }
+    5%   { transform: translate3d(0px, 5%), 0px; }
+    10%   { transform: translate3d(0px, 10%, 0px); }
+    15%   { transform: translate3d(0px, 20%, 0px); }
+    20%   { transform: translate3d(0px, 30%, 0px); }
+    25%   { transform: translate3d(0px, 45%, 0px); }
+    30%   { transform: translate3d(0px, 55%, 0px); }
+    35%   { transform: translate3d(0px, 65%, 0px); }
+    40%   { transform: translate3d(0px, 55%, 0px); }
+    45%   { transform: translate3d(0px, 45%, 0px); }
+    50%   { transform: translate3d(0px, 35%, 0px); }
+    55%   { transform: translate3d(0px, 25%, 0px); }
+    60%   { transform: translate3d(0px, 15%, 0px); }
+    65%   { transform: translate3d(0px, 5%, 0px); }
+    70%   { transform: translate3d(0px, -5%, 0px); }
+    75%   { transform: translate3d(0px,-10%, 0px); }
+    80%   { transform: translate3d(0px,-15%, 0px); }
+    85%   { transform: translate3d(0px,-22.5%, 0px); }
+    90%   { transform: translate3d(0px, -15%, 0px); }
+    95%   { transform: translate3d(0px,-5%, 0px); }  
+  }
+
+  @keyframes moveTextOpposite {
+    0%,100%   { transform: translate3d(0px, 0%, 0px); }
+    5%, 65%   { transform: translate3d(0px, -5%, 0px); }
+    10%   { transform: translate3d(0px, -10%, 0px); }
+    15%   { transform: translate3d(0px, -20%, 0px); }
+    20%   { transform: translate3d(0px, -30%, 0px); }
+    25%, 45%   { transform: translate3d(0px, -45%, 0px); }
+    30%, 40%   { transform: translate3d(0px, -55%, 0px); }
+    35%   { transform: translate3d(0px, -1%, 0px); }
+    50%   { transform: translate3d(0px, -35%, 0px); }
+    55%   { transform: translate3d(0px, -25%, 0px); }
+    60%   { transform: translate3d(0px, -15%, 0px); }
+    70%   { transform: translate3d(0px, 5%), 0px; }
+    75%   { transform: translate3d(0px, 15%, 0px); }
+    80%   { transform: translate3d(0px, 25%, 0px); }
+    85%   { transform: translate3d(0px, 35%, 0px); }
+    90%   { transform: translate3d(0px, 25%, 0px); }
+    95%   { transform: translate3d(0px, 10%, 0px); }  
+  }
+  @-webkit-keyframes moveTextUp {
+    0%,100%   { transform: translate3d(0px, 0%, 0px); }
+    5%   { transform: translate3d(0, -5%, 0px); }
+    10%   { transform: translate3d(0px, -10%, 0px); }
+    15%   { transform: translate3d(0px, -20%, 0px); }
+    20%   { transform: translate3d(0px, -30%, 0px); }
+    25%   { transform: translate3d(0px, -35%, 0px); }
+    30%   { transform: translate3d(0px, -40%, 0px); }
+    35%   { transform: translate3d(0px, -50%, 0px); }
+    40%   { transform: translate3d(0px, -45%, 0px); }
+    45%   { transform: translate3d(0px, -35%, 0px); }
+    50%   { transform: translate3d(0px, -30%, 0px); }
+    55%   { transform: translate3d(0px, -20%, 0px); }
+    60%   { transform: translate3d(0px, -10%, 0px); }
+    65%   { transform: translate3d(0px, -5%, 0px); }
+    70%   { transform: translate3d(0px, 5%, 0px); }
+    75%   { transform: translate3d(0px, 15%, 0px); }
+    80%   { transform: translate3d(0px, 22.5%, 0px); }
+    85%   { transform: translate3d(0px, 15%, 0px); }
+    90%   { transform: translate3d(0px, 10%, 0px); }
+    95%   { transform: translate3d(0px, 5%, 0px); }  
+  }
+
+
+
+</style>
